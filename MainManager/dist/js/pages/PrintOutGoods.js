9a5ff5365d5fcafae74ecb20bf3248f57ccf6be1
151250021 <pc@172.26.212.12>
2017-08-13 18:05:22 +0800

    完成对打印送货单的修改

--- MainManager/dist/js/pages/PrintOutGoods.js
+++ MainManager/dist/js/pages/PrintOutGoods.js
@@ -145,9 +145,11 @@ document.getElementById("businessman").innerHTML = businessman;
 var vm = new Vue({
     el:'#container',
     data:{
-
+        username:''
     },
     mounted(){
+        this.username = this.getCookieValue("username");
+        console.log(this.username+"here");
         if(cname=="") {
             alert("请选择收货单位");
             window.location.href="../Nodes/OutGoods.html"
@@ -188,6 +190,18 @@ var vm = new Vue({
                 }
             }
             return result;
+        }
+    },
+    methods:{
+        getCookieValue:function (cname) {
+            var name = cname + "=";
+            var ca = document.cookie.split(';');
+            for(var i=0; i<ca.length; i++) {
+                var c = ca[i];
+                while (c.charAt(0)==' ') c = c.substring(1);
+                if (c.indexOf(name) != -1) return c.substring(name.length, c.length);
+            }
+            return "";
         }
     }
 });
